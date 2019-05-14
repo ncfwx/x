e3e6b1177ea2be7f321710e062751bc4460323ba
quanhengzhuang <quanhengzhuang@gmail.com>
2019-05-14 16:29:32 +0800

    add get local ip method

---
+++ ip/ip.go
@@ -0,0 +1,23 @@
+package ip
+
+import (
+	"errors"
+	"net"
+)
+
+func GetLocalIp() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", errors.New("get local ip failed")
+}
